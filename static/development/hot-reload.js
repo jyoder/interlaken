ec26d1bbf30414222e08c518a382775048d28561
John Yoder <jcyoder@gmail.com>
2022-06-29 18:31:34 -0700

    Improve speed of hot-reload
    Browsers implement an exponential backoff for websocket reconnects.
    This change avoids websocket reconnects and instead switches to fetch-
    based polling when the websocket connection breaks.

--- static/development/hot-reload.js
+++ static/development/hot-reload.js
@@ -1,21 +1,19 @@
 function startHotReloadMonitor() {
   const host = window.location.host;
-  const url = `ws://${host}`;
-
-  const initialRestartBackoff = 0.1;
-  const maxRestartBackoff = 20;
+  const webSocketUrl = `ws://${host}`;
+  const pollUrl = `http://${host}`;
+  const pollDelay = 0.1;
 
   let status = "initialized";
   let webSocket = null;
-  let restartBackoff = initialRestartBackoff;
-  let nextRestart = new Date();
+  let nextPoll = new Date();
 
   setInterval(() => {
     if (status === "initialized") {
       console.log("hot-reload: establishing websocket connection");
       status = "starting";
 
-      webSocket = new WebSocket(url);
+      webSocket = new WebSocket(webSocketUrl);
       webSocket.onopen = () => {
         console.log("hot-reload: connection established");
         status = "started";
@@ -30,36 +28,18 @@ function startHotReloadMonitor() {
       };
     } else if (status === "disconnected") {
       const now = new Date();
-      if (nextRestart > now) {
+      if (nextPoll > now) {
         return;
       }
-      console.log("hot-reload: attempting websocket reconnect");
-      status = "restarting";
-
-      webSocket = new WebSocket(url);
-      nextRestart = addSeconds(now, restartBackoff);
-      const nextBackoff = restartBackoff * 2;
-      restartBackoff = nextBackoff > maxRestartBackoff ? maxRestartBackoff : nextBackoff;
-
-      webSocket.onopen = () => {
-        if (status !== "restarting") {
-          return;
-        }
-
-        console.log("hot-reload: connection re-established, reloading page");
-        status = "restarted";
-        window.location.reload();
-      };
-
-      webSocket.onclose = () => {
-        if (status !== "restarting" && status !== "restarted") {
-          return;
-        }
-        console.log(
-          `hot-reload: websocket disconnected, attempting reconnect in ${restartBackoff} seconds`
-        );
-        status = "disconnected";
-      };
+      status = "polling";
+      nextPoll = addSeconds(now, pollDelay);
+      fetch(pollUrl)
+        .then((_) => {
+          window.location.reload();
+        })
+        .catch((_) => {
+          status = "disconnected";
+        });
     }
   }, 100);
 
